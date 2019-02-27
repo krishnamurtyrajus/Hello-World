60996c7b8f22a977296d227fa5d75f5a8462b8d1
krishnamurtyrajus <48060559+krishnamurtyrajus@users.noreply.github.com>
2019-02-27 21:12:28 +0530

    Add files via upload
    test

---
+++ FirstClass.java
@@ -0,0 +1,24 @@
+
+public class FirstClass{
+
+int a;
+
+public static void main(String[]args){
+
+FirstClass tester=new FirstClass();
+
+System.out.print(tester);
+
+System.out.print("\n");
+
+System.out.print(tester.a);
+
+tester.add();
+}
+
+public void add(){
+
+
+}
+
+}
